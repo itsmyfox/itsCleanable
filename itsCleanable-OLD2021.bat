771dd8b227d53760c42cf0082c8c32675ba601eb
itsmyfox <81083006+itsmyfox@users.noreply.github.com>
2025-04-19 02:16:02 +0300

    Update itsCleanable-OLD2021.bat
    Старый скрипт до глобальных изменений в 2025 году.

--- itsCleanable-OLD2021.bat
+++ itsCleanable-OLD2021.bat
@@ -1,131 +1,129 @@
 @echo off
-chcp 866
-set Username=‘ˆ‘’…Œ€
+chcp 1251
+set Username=Администратор
 set Version=1.1.12
 set Dates=21.05.2021
-title Žç¨áâª  ¤¨áª  "itsCleanable | Version: %Version% | Update %Dates%
+title Очистка диска "itsCleanable | Version: %Version% | Update %Dates%
 
-echo —¥à¥§ 7 á¥ªã­¤ § ¯ãáâ¨âìáï áªà¨¯â 'itsCleanable.bat / Version %Version% / Ž¡­®¢«¥­¨¥ ®â %Dates%' - ®ç¨áâª¨ ¤¨áª  ®â ¢à¥¬¥­­ëå ¨ ­¥¨á¯®«ì§ã¥¬ëå ä ©«®¢. …á«¨ ­¥ å®â¨â¥ íâ®£® ¤¥« âì, ­ ¦¬¨â¥ ª®¬¡¨­ æ¨î 'ctrl + c'
+echo Через 7 секунд запустится скрипт 'itsCleanable.bat / Version %Version% / Обновление от %Dates%' - очистки диска от временных и неиспользуемых файлов. Если не хотите этого делать, нажмите комбинацию 'ctrl + c'
 
 For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a-%%b)
 
-chcp 1251
 mkdir C:\LogsCache\
 fsutil volume diskfree C:\ > C:\LogsCache\log-volume-start-itsCleanable_%date:~0,2%-%date:~3,2%-%date:~8,2%_%mytime%.txt
-chcp 866
 fsutil volume diskfree C:\
 
 timeout 7 /nobreak
 
 echo ===
-echo ========== Žáâ ­®¢ª  á«ã¦¡ ­  ¬®¬¥­â à ¡®âë áªà¨¯â  ==========
+echo ========== Остановка служб на момент работы скрипта ==========
 echo ===
 
-echo Žáâ ­®¢ª  á«ã¦¡ë '–¥­âà ®¡­®¢«¥­¨ï Windows'
+echo Остановка службы 'Центр обновления Windows'
 timeout 2
 net stop wuauserv
 
 echo ===
-echo ========== Žáâ ­®¢ª  ¤àã£¨å á«ã¦¡ ==========
+echo ========== Остановка других служб ==========
 echo ===
 
 timeout 1
 echo XBOX
 net stop "Xbox Accessory Management Service"
 timeout 1
-echo ¨®¬¥âà¨ç¥áª ï á«ã¦¡  Windows
+echo Биометрическая служба Windows
 net stop "WbioSrvc"
 timeout 1
-echo „¨á¯¥âç¥à ¯à®¢¥àª¨ ¯®¤«¨­­®áâ¨ Xbox Live
+echo Диспетчер проверки подлинности Xbox Live
 net stop "XblAuthManager"
 timeout 1
-echo ®¤¨â¥«ìáª¨© ª®­âà®«ì
+echo Родительский контроль
 net stop "WpcMonSvc"
 timeout 1
-echo ‘¥â¥¢ ï á«ã¦¡  Xbox Live
+echo Сетевая служба Xbox Live
 net stop "XboxNetApiSvc"
 timeout 1
-echo ‘«ã¦¡  Windows License Manager
+echo Служба Windows License Manager
 net stop "LicenseManager"
 timeout 1
 echo Windows Mobile Hotspot
 net stop "icssvc"
 timeout 1
-echo ‘«ã¦¡  ¤¥¬®­áâà æ¨¨ ¬ £ §¨­ 
+echo Служба демонстрации магазина
 net stop "RetailDemo"
 timeout 1
-echo ‘«ã¦¡  ã¯à ¢«¥­¨ï à ¤¨®
+echo Служба управления радио
 net stop "RmSvc"
 timeout 1
-echo ” ªá
+echo Факс
 net stop "Fax"
 
 echo ===
-echo ========== Žáâ ­®¢ª  á«ã¦¡ë ¯®¨áª  ¤«ï ã¤ «¥­¨ï ä ©«  ==========
+echo ========== Остановка службы поиска для удаления файла ==========
 echo ===
 
 echo ===
-echo Windows.edb ï¢«ï¥âáï ¨­¤¥ªá­®© ¡ §®© ¤ ­­ëå á«ã¦¡ë ¯®¨áª  Windows. « £®¤ àï â ª®© ¨­¤¥ªá æ¨¨ ¯®¨áª ¯à®¨áå®¤¨â ¡ëáâà¥¥ ¨ íää¥ªâ¨¢­¥¥. ¥®¡å®¤¨¬® «¨ ã¤ «ïâì Windows.edb ¨ £¥­¥à¨à®¢ âì ­®¢ë© ä ¨« ¤«ï ®á¢®¡®¦¤¥­¨ï ¯à®áâà ­áâ¢ ? (Y/N)
-choice /m "Windows.edb ï¢«ï¥âáï ¨­¤¥ªá­®© ¡ §®© ¤ ­­ëå á«ã¦¡ë ¯®¨áª  Windows. « £®¤ àï â ª®© ¨­¤¥ªá æ¨¨ ¯®¨áª ¯à®¨áå®¤¨â ¡ëáâà¥¥ ¨ íää¥ªâ¨¢­¥¥. ¥®¡å®¤¨¬® «¨ ã¤ «ïâì Windows.edb ¨ £¥­¥à¨à®¢ âì ­®¢ë© ä ¨« ¤«ï ®á¢®¡®¦¤¥­¨ï ¯à®áâà ­áâ¢ ?"
+echo Windows.edb является индексной базой данных службы поиска Windows. Благодаря такой индексации поиск происходит быстрее и эффективнее. Необходимо ли удалять Windows.edb и генерировать новый файл для освобождения пространства? (Y/N)
+choice /m "Windows.edb является индексной базой данных службы поиска Windows. Благодаря такой индексации поиск происходит быстрее и эффективнее. Необходимо ли удалять Windows.edb и генерировать новый файл для освобождения пространства?"
 
 if errorlevel 2 goto N
 if errorlevel 1 goto Y
 
 :N
 timeout 2
-echo ‚ë ®âª § «¨áì ®â ã¤ «¥­¨ï ä ©«  Windows.edb (“¤ «¥­¨ï ¨­¤¥ªá­®© ¡ §ë ¤ ­­ëå á«ã¦¡ë ¯®¨áª  Windows).
+echo Вы отказались от удаления файла Windows.edb (Удаления индексной базы данных службы поиска Windows).
 goto NOWINDOWSEDB
 
 :Y
 
-echo Žáâ ­®¢ª  á«ã¦¡ë '®¨áª Windows'
+echo Остановка службы 'Поиск Windows'
 timeout 2
 net stop "Windows Search"
-echo “¤ «¥­¨¥ ä ©«  Windows.edb ¨ £¥­¥à æ¨ï ­®¢®£® ä ©« .
+echo Удаление файла Windows.edb и генерация нового файла.
 timeout 2
 REG ADD "HKLM\SOFTWARE\Microsoft\Windows Search" /v SetupCompletedSuccessfully /t REG_DWORD /d 0 /f
 del %PROGRAMDATA%\Microsoft\Search\Data\Applications\Windows\Windows.edb
 timeout 2
 
 echo ===
-echo ========== ‡ ¯ãáª á«ã¦¡ë ¯®¨áª  ¤«ï £¥­¥à æ¨¨ ä ©«  ==========
+echo ========== Запуск службы поиска для генерации файла ==========
 echo ===
 
-echo ‡ ¯ãáª á«ã¦¡ë '®¨áª Windows'
+echo Запуск службы 'Поиск Windows'
 net start "Windows Search"
 
 :NOWINDOWSEDB
 
 echo ===
-echo Žâªàë¢ ¥â ®ª­® 'Žç¨áâª  ¤¨áª ' ¨ á¯à è¨¢ ¥â, çâ® å®â¨â¥ ã¤ «¨âì. ‘«¥¤ã¥â ¯à®áâ ¢¨âì £ «®çª¨.
+echo Открывает окно 'Очистка диска' и спрашивает, что хотите удалить. Следует проставить галочки.
 timeout 2
 %windir%\system32\cmd.exe /c "start cleanmgr /sageset:64541
 
 echo ===
-echo ¥®¡å®¤¨¬ë¥ £ «®çª¨ ¯à®áâ ¢¨«? (Y/N)
-choice /m "¥®¡å®¤¨¬ë¥ £ «®çª¨ ¯à®áâ ¢¨«?"
+echo Необходимые галочки проставил? (Y/N)
+choice /m "Необходимые галочки проставил?"
 
 if errorlevel 2 goto N
 if errorlevel 1 goto Y
 
 :N
 timeout 2
-echo ‚ë ®âª § «¨áì ®â ®ç¨áâª¨ ¤¨áª .
+echo Вы отказались от очистки диска.
 goto COMPLETE
 
 :Y
 
 timeout 2
-echo à®¤®«¦ ¥¬ ¯à®¨§¢®¤¨âì ®ç¨áâªã ¤¨áª ...
-echo ®¤®¦¤¨â¥, à ¡®â ¥â ª®¬ ­¤  cleanmgr.exe /SETUP
+echo Продолжаем производить очистку диска...
+echo Подождите, работает команда cleanmgr.exe /SETUP
 cleanmgr.exe /SETUP
-echo ‡ ¯ãáª ¥âáï cleanmgr...
+echo Запускается cleanmgr...
 %windir%\system32\cmd.exe /c "start cleanmgr /sagerun:64541
 
 :COMPLETE
 
 echo ===
-echo ========== ‚ë¤ î ®á®¡ë¥ ¯à ¢  ­  ¢à¥¬¥­­ë¥ ¯ ¯ª¨ ==========
+echo ========== Выдаю особые права на временные папки ==========
 echo ===
 timeout 2
 %PROGRAMDATA%\Microsoft\Search\Data\Applications\Windows\Windows.edb
@@ -143,7 +141,7 @@ TAKEOWN /f "%WINDIR%\assembly\tmp" /r /d y && ICACLS "%WINDIR%\assembly\tmp" /gr
 TAKEOWN /f "%WINDIR%\SoftwareDistribution\Download\SharedFileCache" /r /d y && ICACLS "%WINDIR%\SoftwareDistribution\Download\SharedFileCache" /grant %Username%:F /t
 TAKEOWN /f "%USERPROFILE%\AppData\Local\Temp" /r /d y && ICACLS "%USERPROFILE%\AppData\Local\Temp" /grant %Username%:F /t
 TAKEOWN /f "C:\Users\User\AppData\Local\Temp" /r /d y && ICACLS "C:\Users\User\AppData\Local\Temp" /grant %Username%:F /t
-TAKEOWN /f "C:\Users\€¤¬¨­¨áâà â®à\AppData\Local\Temp" /r /d y && ICACLS "C:\Users\€¤¬¨­¨áâà â®à\AppData\Local\Temp" /grant %Username%:F /t
+TAKEOWN /f "C:\Users\Администратор\AppData\Local\Temp" /r /d y && ICACLS "C:\Users\Администратор\AppData\Local\Temp" /grant %Username%:F /t
 TAKEOWN /f "C:\ProgramData\Microsoft\Diagnosis" /r /d y && ICACLS "C:\ProgramData\Microsoft\Diagnosis" /grant %Username%:F /t
 TAKEOWN /f "C:\ProgramData\Kaspersky Lab\KES\Temp" /r /d y && ICACLS "C:\ProgramData\Kaspersky Lab\KES\Temp" /grant %Username%:F /t
 TAKEOWN /f "C:\ProgramData\Kaspersky Lab\KES\Cache" /r /d y && ICACLS "C:\ProgramData\Kaspersky Lab\KES\Cache" /grant %Username%:F /t
@@ -176,46 +174,46 @@ TAKEOWN /f "C:\ProgramData\Crypto Pro\Installer Cache" /r /d y && ICACLS "C:\Pro
 TAKEOWN /f "%USERPROFILE%\AppData\Local\Yandex\YandexBrowser\Application" /r /d y && ICACLS "%USERPROFILE%\AppData\Local\Yandex\YandexBrowser\Application" /grant %Username%:F /t
 
 echo ===
-echo ========== Žç¨áâª  ¤¨áª  ®â ¢à¥¬¥­­ëå ä ©«®¢ ==========
+echo ========== Очистка диска от временных файлов ==========
 echo ===
 
 echo ===
-echo ========== Žç¨áâª  ¢ ¯ ¯ª¥ "C:\Windows" ==========
+echo ========== Очистка в папке "C:\Windows" ==========
 echo ===
 
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¢ ¤¨à¥ªâ®à¨¨ 'C:\Windows\Logs\' ¨ 'C:\logs\'
+echo Удаление временных файлов в директории 'C:\Windows\Logs\' и 'C:\logs\'
 timeout 2
 del "%WINDIR%\Logs\*" /s /f /q
 del "C:\logs\*" /s /f /q
 mkdir C:\Logs\
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¢ ¤¨à¥ªâ®à¨¨ 'C:\Windows\Temp'
+echo Удаление временных файлов в директории 'C:\Windows\Temp'
 timeout 2
 del "%WINDIR%\Temp\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¢ ¤¨à¥ªâ®à¨¨ 'C:\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Logs\*'
+echo Удаление временных файлов в директории 'C:\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Logs\*'
 timeout 2
 del "%WINDIR%\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Logs\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¢ ¤¨à¥ªâ®à¨¨ 'C:\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Cache\*'
+echo Удаление временных файлов в директории 'C:\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Cache\*'
 timeout 2
 del "%WINDIR%\ServiceProfiles\NetworkService\AppData\Local\Microsoft\Windows\DeliveryOptimization\Cache\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\Windows\SoftwareDistribution\Download\*'
+echo Удаление временных файлов по директории: 'C:\Windows\SoftwareDistribution\Download\*'
 timeout 2
 del "%WINDIR%\SoftwareDistribution\Download\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\Windows\Installer\$PatchCache$\*'
+echo Удаление временных файлов по директории: 'C:\Windows\Installer\$PatchCache$\*'
 timeout 2
 del "%WINDIR%\Installer\$PatchCache$\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\Windows\assembly\NativeImages_*\temp\*'
+echo Удаление временных файлов по директории: 'C:\Windows\assembly\NativeImages_*\temp\*'
 timeout 2
 del "%WINDIR%\assembly\NativeImages_v2.0.50727_32\temp\*" /s /f /q
 del "%WINDIR%\assembly\NativeImages_v4.0.30319_64\temp\*" /s /f /q
@@ -225,164 +223,164 @@ del "%WINDIR%\assembly\temp\*" /s /f /q
 del "%WINDIR%\assembly\tmp\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\Windows\SoftwareDistribution\Download\SharedFileCache'
+echo Удаление временных файлов по директории: 'C:\Windows\SoftwareDistribution\Download\SharedFileCache'
 timeout 2
 del "%WINDIR%\SoftwareDistribution\Download\SharedFileCache\*" /s /f /q
 
 echo ===
-echo ========== Žç¨áâª  ¢ ¯ ¯ª å "AppData" ==========
+echo ========== Очистка в папках "AppData" ==========
 echo ===
 
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: '\AppData\Local\Temp\*'
+echo Удаление временных файлов по директории: '\AppData\Local\Temp\*'
 timeout 2
 del "%USERPROFILE%\AppData\Local\Temp\*" /s /f /q
 
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: '\AppData\Local\Yandex\YandexBrowser\'
+echo Удаление временных файлов по директории: '\AppData\Local\Yandex\YandexBrowser\'
 timeout 2
 del "%USERPROFILE%\AppData\Local\Yandex\YandexBrowser\Application\browser.7z" /s /f /q
 del "%USERPROFILE%\AppData\Local\Yandex\YandexBrowser\Application\brand-package.cab" /s /f /q
 del "%USERPROFILE%\AppData\Local\Yandex\YandexBrowser\Application\setup.exe" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\Users\User\AppData\Local\Temp\*'
+echo Удаление временных файлов по директории: 'C:\Users\User\AppData\Local\Temp\*'
 timeout 2
 del "C:\Users\User\AppData\Local\Temp\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\Users\€¤¬¨­¨áâà â®à\AppData\Local\Temp\*'
+echo Удаление временных файлов по директории: 'C:\Users\Администратор\AppData\Local\Temp\*'
 timeout 2
-del "C:\Users\€¤¬¨­¨áâà â®à\AppData\Local\Temp\*" /s /f /q
+del "C:\Users\Администратор\AppData\Local\Temp\*" /s /f /q
 
 echo ===
-echo ========== Žç¨áâª  ¢ ¯ ¯ª¥ "ProgramData" ==========
+echo ========== Очистка в папке "ProgramData" ==========
 echo ===
 
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¢ ¤¨à¥ªâ®à¨¨ 'C:\ProgramData\Crypto Pro\Installer Cache\*'
+echo Удаление временных файлов в директории 'C:\ProgramData\Crypto Pro\Installer Cache\*'
 timeout 2
 del "C:\ProgramData\Crypto Pro\Installer Cache\*" /s /f /q
 
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¢ ¤¨à¥ªâ®à¨¨ 'C:\ProgramData\Security Code\Secret Net Studio\localcache\patch.exe' ¨ 'ˆé¥â ¯® ¢á¥© ¯ ¯ª¥ ¨ ã¤ «ï¥â patch.exe'
+echo Удаление временных файлов в директории 'C:\ProgramData\Security Code\Secret Net Studio\localcache\patch.exe' и 'Ищет по всей папке и удаляет patch.exe'
 timeout 2
 del "C:\ProgramData\Security Code\Secret Net Studio\localcache\patch.exe" /s /f /q
 
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¢ ¤¨à¥ªâ®à¨¨ 'C:\ProgramData\Aktiv Co\*'
+echo Удаление временных файлов в директории 'C:\ProgramData\Aktiv Co\*'
 timeout 2
 del "C:\ProgramData\Aktiv Co\*" /s /f /q
 
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¢ ¤¨à¥ªâ®à¨¨ 'C:\ProgramData\USOShared\Logs\*'
+echo Удаление временных файлов в директории 'C:\ProgramData\USOShared\Logs\*'
 timeout 2
 del "C:\ProgramData\USOShared\Logs\*" /s /f /q
 
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¢ ¤¨à¥ªâ®à¨¨ 'C:\ProgramData\VMware\VDM\logs' ¨ 'C:\ProgramData\VMware\VDM\Dumps\'
+echo Удаление временных файлов в директории 'C:\ProgramData\VMware\VDM\logs' и 'C:\ProgramData\VMware\VDM\Dumps\'
 timeout 2
 del "C:\ProgramData\VMware\VDM\logs\*" /s /f /q
 del "C:\ProgramData\VMware\VDM\Dumps\*" /s /f /q
 
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\Microsoft\Diagnosis\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\Microsoft\Diagnosis\*'
 timeout 2
 del "C:\ProgramData\Microsoft\Diagnosis\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\Kaspersky Lab\KES\Temp\* ¨ C:\ProgramData\Kaspersky Lab\KES\Cache\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\Kaspersky Lab\KES\Temp\* и C:\ProgramData\Kaspersky Lab\KES\Cache\*'
 timeout 2
 del "C:\ProgramData\Kaspersky Lab\KES\Temp\*" /s /f /q
 del "C:\ProgramData\Kaspersky Lab\KES\Cache\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\KasperskyLab\adminkit\1103\$FTCITmp\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\KasperskyLab\adminkit\1103\$FTCITmp\*'
 timeout 2
 del "C:\ProgramData\KasperskyLab\adminkit\1103\$FTCITmp\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\Intel\Logs\*' ¨ 'C:\ProgramData\Intel\Package Cache\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\Intel\Logs\*' и 'C:\ProgramData\Intel\Package Cache\*'
 timeout 2
 del "C:\ProgramData\Intel\Logs\*" /s /f /q
 del "C:\ProgramData\Intel\Package Cache\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\Veeam\Setup\Temp\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\Veeam\Setup\Temp\*'
 timeout 2
 del "C:\ProgramData\Veeam\Setup\Temp\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\Crypto Pro\Installer Cache\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\Crypto Pro\Installer Cache\*'
 timeout 2
 del "C:\ProgramData\Crypto Pro\Installer Cache\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\Package Cache\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\Package Cache\*'
 timeout 2
 del "C:\ProgramData\Package Cache\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\Oracle\Java\installcache_x64\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\Oracle\Java\installcache_x64\*'
 timeout 2
 del "C:\ProgramData\Oracle\Java\installcache_x64\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\LANDesk\Log\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\LANDesk\Log\*'
 timeout 2
 del "C:\ProgramData\LANDesk\Log\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\Intel\Logs\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\Intel\Logs\*'
 timeout 2
 del "C:\ProgramData\Intel\Logs\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\ProgramData\LANDesk\Temp\*'
+echo Удаление временных файлов по директории: 'C:\ProgramData\LANDesk\Temp\*'
 timeout 2
 del "C:\ProgramData\LANDesk\Temp\*" /s /f /q
 
 echo ===
-echo ========== Žç¨áâª  ¢ ¤¨áª¥ "C:\" ==========
+echo ========== Очистка в диске "C:\" ==========
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\Intel\*'
+echo Удаление временных файлов по директории: 'C:\Intel\*'
 timeout 2
 del "C:\Intel\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\SWSetup\*'
+echo Удаление временных файлов по директории: 'C:\SWSetup\*'
 timeout 2
 del "C:\SWSetup\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\MSOCache\All Users\*'
+echo Удаление временных файлов по директории: 'C:\MSOCache\All Users\*'
 timeout 2
 del "C:\MSOCache\All Users\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¢à¥¬¥­­ëå ä ©«®¢ ¯® ¤¨à¥ªâ®à¨¨: 'C:\AMD'
+echo Удаление временных файлов по директории: 'C:\AMD'
 timeout 2
 del "C:\AMD\*" /s /f /q
 
 echo ===
-echo “¤ «¥­¨¥ ¯ ¯ª¨ ¯® ¤¨à¥ªâ®à¨¨: 'C:\$WINDOWS.~BT'
+echo Удаление папки по директории: 'C:\$WINDOWS.~BT'
 timeout 2
 rd "C:\$WINDOWS.~BT" /s /q
 
 echo ===
-echo “¤ «¥­¨¥ ¯ ¯ª¨ ¯® ¤¨à¥ªâ®à¨¨: 'C:\Windows10Upgrade'
+echo Удаление папки по директории: 'C:\Windows10Upgrade'
 timeout 2
 rd "C:\Windows10Upgrade" /s /q
 
 echo ===
-echo ========== ‡ ¯ãáª á«ã¦¡ ==========
+echo ========== Запуск служб ==========
 echo ===
 
-echo ‡ ¯ãáª á«ã¦¡ë '–¥­âà ®¡­®¢«¥­¨ï Windows'
+echo Запуск службы 'Центр обновления Windows'
 timeout 2
 net start wuauserv
 
 echo ===
-echo ‡ ¢¥àè¥­¨ï à ¡®âë...
-echo itsCleanable 'Version: %Version% / Ž¡­®¢«¥­¨¥ ®â: %Dates%'
-echo €¢â®à: ‡ å à®¢ ˆ«ìï €«¥ªá¥¥¢¨ç.
-echo ®¢ ï ¢¥àá¨ï github.com/itsmyfox ¢ à §¤¥«¥ 'itsCleanable'
-echo ‘¢®¨ ¨¤¥¨ ‚ë ¬®¦¥â¥ ¯à¥¤«®¦¨âì ¢ à §¤¥«¥ 'pull requests'
+echo Завершения работы...
+echo itsCleanable 'Version: %Version% / Обновление от: %Dates%'
+echo Автор: Захаров Илья Алексеевич.
+echo Новая версия github.com/itsmyfox в разделе 'itsCleanable'
+echo Свои идеи Вы можете предложить в разделе 'pull requests'
 
 echo ===
-echo Ž¦¨¤ ¥¬ § ¢¥àè¥­¨ï ®ç¨áâª¨ ¤¨áª ...
+echo Ожидаем завершения очистки диска...
 timeout 7
 
-pause
+pause 
